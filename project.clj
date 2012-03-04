ffd41e6dd75cd6823cb563a19a5d0ec8f58f7e4a
Brad Lucas <brad@beaconhill.com>
2012-03-04 08:17:43 -0500

    correct linkedin-connections dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.blucas/toplinked "1.0.0"
   :description "Toplinked List Processing"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.blucas/linkedin-connecions "1.0.0"]]
+                 [org.clojars.blucas/linkedin-connections "1.0.0"]]
   :main toplinked.core)
