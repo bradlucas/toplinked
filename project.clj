677c2ebd13e09c9087e595ce34c865a882012f81
Brad Lucas <brad@beaconhill.com>
2020-11-15 22:47:26 -0500

    new group com.bradlucas

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blucas/toplinked "1.0.1"
+(defproject com.bradlucas/toplinked "1.0.1"
   :description "Toplinked List Processing"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.blucas/linkedin-connections "1.0.2"]]
